370504e52e21badfe805283c7c0a1ab226548ffa
pancakes <41842151+pancakes2@users.noreply.github.com>
2024-05-08 22:38:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -131,13 +131,13 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-    for (long i = map->current; i < map->capacity; i++) {
-        if (map->buckets[i] != NULL) {
+    for (long i = 0; i < map->capacity; i++) {
+        if (map->buckets[i] != NULL && strcmp(map->buckets[i]->key, "DELETED") != 0) {
             map->current = i;
             return map->buckets[i];
         }
     }
-    return NULL; // No non-NULL pairs found
+    return NULL; // No valid Pair found
 }
 
 Pair * nextMap(HashMap * map) {
