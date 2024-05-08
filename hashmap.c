975a0274c6288fb8797f8c9fbbbec8be8b0c0707
pancakes <41842151+pancakes2@users.noreply.github.com>
2024-05-08 22:30:55 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -131,7 +131,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-    for (long i = 0; i < map->capacity; i++) {
+    for (long i = map->current; i < map->capacity; i++) {
         if (map->buckets[i] != NULL) {
             map->current = i;
             return map->buckets[i];
